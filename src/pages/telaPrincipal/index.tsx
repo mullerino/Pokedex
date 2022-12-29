c762cf54c14ee6a1e868430cdb970d460a2ffe2b
Leandro <leandromllr9@gmail.com>
2022-12-28 22:33:11 -0300

    feat: funcionamento do input

--- src/pages/telaPrincipal/index.tsx
+++ src/pages/telaPrincipal/index.tsx
@@ -1,46 +1,56 @@
-import { Row, Col } from "antd";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { Row, Col, Result } from "antd";
 
 import CardPokemon from "./componentes/cardPokemon";
 import { Pokemon } from "../../@types/pokemon";
 
+import "./index.css"
+import CampoText from "./componentes/campoText";
+
+
 const App = (): JSX.Element => {
-    const listPokemons: Pokemon[] = [
-        {
-          img: "",
-          cod: "001",
-          nome: "Bulbasaur",
-          caracteristicas: ["Grass", "Poison"],
-        },
-        {
-          img: "",
-          cod: "002",
-          nome: "Charmander",
-          caracteristicas: ["Fire"],
-        },
-        {
-          img: "",
-          cod: "003",
-          nome: "Blastoise",
-          caracteristicas: ["Water"],
-        },
-        {
-          img: "",
-          cod: "004",
-          nome: "Ivysaur",
-          caracteristicas: ["Grass", "Poison"],
-        },
-      ];
-      
+
+    const [textDigitado, setTextDigitado] = useState<string>('')
+    const [data,setData] = useState<Pokemon[]>([])
+    const [pokemonsFiltrados, setPokemonsFiltrados] = useState<Pokemon[]>([])
+
+    useEffect(() => {
+      axios.get
+      ('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json')
+      .then(({data}) => {
+        setData(data.pokemon)
+      })
+      .catch((error) => {console.log(error)})
+    },[]) 
+
+    const namePoks = data.map(item=> item.name.toLowerCase())
+
+    const pokFilter = namePoks.filter((nome) => nome.includes(textDigitado.toLowerCase()))
+
+    console.log(pokFilter)
+
+    //setPokemonsFiltrados(data.filter((pokemons) => pokemons.name.includes(pokFilter)))
+
     return (
         <div className="container-main">
-            <h1>Cabecalho</h1>
+            <section className="logo">
+              <img src="/logo_pokemon.png" alt="Logo"/>
+            </section>
+            <section>
+              <CampoText buscarPokemons={textInput => setTextDigitado(textInput)}/>
+            </section>
             <div className="container-list">
               <Row gutter={[15,15]} className = "row">
-                {listPokemons.map((pokemon) => (
-                  <Col span={6} key={pokemon.cod}>
-                    <CardPokemon objeto={pokemon}/>
-                  </Col>
-                ))}
+                {data.map((pokemon) => {
+                  if(pokemon.name.toLowerCase().includes(textDigitado.toLowerCase())){
+                    return (
+                      <Col span={6} key={pokemon.id}>
+                          <CardPokemon objeto={pokemon}/>
+                      </Col>
+                    )
+                }})}
               </Row>
             </div>
         </div>
