46c928b631620a406f63da3def2785353108dcfd
Leandro <leandromllr9@gmail.com>
2023-02-11 22:25:49 -0300

    feat: adicionando condições para renderização das info sobre evoluções

--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -7,8 +7,9 @@ interface IDetailsPage extends Pokemon{}
 
 const { Header, Content, Footer } = Layout;
 
-const DetailsPage = ({ name,num, img, egg, type, height, weight, weaknesses, spawn_chance, next_evolution } : IDetailsPage)=> {
+const DetailsPage = ({ name,num, img, egg, type, height, weight, weaknesses, spawn_chance, next_evolution, prev_evolution } : IDetailsPage)=> {
 
+    const typeEvolution = next_evolution == null ? 'Prev evolution' : 'Next evolution'
 
     return (
         <Layout style={{ minHeight: '100vh' }}>
@@ -23,40 +24,63 @@ const DetailsPage = ({ name,num, img, egg, type, height, weight, weaknesses, spa
                         <section className='content-details'>
                             <div className='details-img'>
                                 <img src={img}></img>
+                                <div className='details-evolution'>
+                                    <h3>{typeEvolution}</h3> 
+                                    {next_evolution != null ? next_evolution.map(item =>{
+                                        return(
+                                            <Link key={item.num} to={`/${item.name}`}><span>{item.name}</span></Link>
+                                        )
+                                    })
+                                    : prev_evolution.map(item =>{
+                                        return (
+                                            <Link  key={item.num} to={`/${item.name}`}><span>{item.name}</span></Link>
+                                        )
+                                    }) 
+                                    }
+                                </div>
                             </div>
                             <div className='details-info'>
                                 <div className='info-card'>
-                                    <div className='info-card-height'>
-                                        <span>Height</span> 
+                                    <div>
+                                        <h3>Height</h3> 
                                         <span>{height}</span>
                                     </div>
                                     <div>
-                                        <span>Weight</span> 
+                                        <h3>Weight</h3> 
                                         <span>{weight}</span>
                                     </div>
                                     <div>
-                                        <span>Egg</span> 
+                                        <h3>Egg</h3> 
                                         <span>{egg}</span>
                                     </div>
-                                    <div>
-                                        <span>Next evolution</span> 
-                                        {next_evolution.map(item =>{
-                                            return(
-                                                <span key={item.num}>{item.name}</span>
+                                    <div className='details-weakness'>
+                                        <h3>Weakness</h3>
+                                        {weaknesses.map(item =>{
+                                            return (
+                                                <span key={item}>{item}</span>
                                             )
                                         })}
                                     </div>
                                     <div>
-                                        <span>Spawn chances</span> 
+                                        <h3>Spawn chances</h3> 
                                         <span>{spawn_chance}</span>
                                     </div>
+                                    <div>
+                                        <h3>{`Type${type.length>1 ? 's' : ''}`}</h3>
+                                        {type.map(item =>{
+                                            return (
+                                                <span key={item}>{item}</span>
+                                            )
+                                        })}
+                                    </div>
+                                
                                 </div>
                             </div>
                         </section>
                     </div>
                 </Content>
                 <Footer style={{background: "#C6E1F5", textAlign: 'center'}}>
-                    <Link to={"/"}><button type='button' className='footer-button'>Voltar</button></Link>
+                    <Link to={"/"}><button type='button' className='footer-button'>Voltar para tela inicial</button></Link>
                 </Footer>
             </Layout>
         </Layout>
